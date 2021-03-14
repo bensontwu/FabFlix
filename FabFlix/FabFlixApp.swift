8e38841ffccc22a431641a28f13bd3e31dc936f2
Benson Wu <wubt@uci.edu>
2021-03-13 16:22:03 -0800

    initial commit

---
+++ FabFlix/FabFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabFlixApp.swift
+//  FabFlix
+//
+//  Created by Benson  on 3/13/21.
+//
+
+import SwiftUI
+
+@main
+struct FabFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
